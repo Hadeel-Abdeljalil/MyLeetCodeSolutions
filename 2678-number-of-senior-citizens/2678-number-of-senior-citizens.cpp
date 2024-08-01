1d8ed9d45b1ea38ec065c249ce22425dc1d290e5
Hadeel Abdeljalil <h.02.adeel@gmail.com>
2024-08-01 10:57:38 +0300

    Time: 8 ms (47.01%), Space: 17.5 MB (31.37%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -3,7 +3,7 @@ public:
     int countSeniors(vector<string>& details) {
         int count=0;
         for(int i=0;i<details.size();i++){
-            int age=(details[i][11]-'0')*10 + details[i][12]-'0';
+            int age=(details[i][11]-'0')*10 + details[i][12]-'0';//convert from ASCII to a real number
             if(age>60){
                 count++;
             }
